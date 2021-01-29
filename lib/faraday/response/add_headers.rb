e6e7b8dc61979f3bada902940036c95d95d503c6
Alan Locke <alan@pipelinedeals.com>
2021-01-29 16:53:21 -0600

    UnBind

--- lib/faraday/response/add_headers.rb
+++ lib/faraday/response/add_headers.rb
@@ -2,7 +2,6 @@ module Faraday
   class Response::AddHeaders < Response::Middleware
     def on_complete(env)
       env.body[:http_headers] = env.response_headers
-      binding.pry
       env.body
     end
   end
