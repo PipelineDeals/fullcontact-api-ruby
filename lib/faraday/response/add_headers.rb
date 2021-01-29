ec26315759b840cacabfd47b0b55d12bae726edd
Alan Locke <alan@pipelinedeals.com>
2021-01-29 16:43:44 -0600

    Bind

--- lib/faraday/response/add_headers.rb
+++ lib/faraday/response/add_headers.rb
@@ -2,6 +2,7 @@ module Faraday
   class Response::AddHeaders < Response::Middleware
     def on_complete(env)
       env.body[:http_headers] = env.response_headers
+      binding.pry
       env.body
     end
   end
